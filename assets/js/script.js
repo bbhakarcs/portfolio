2bf8eaf6cb3e7078a0d4c8d9528235534595d2cf
bbhakarcs <bbhakarcs@gmail.com>
2026-03-09 16:49:25 +0530

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2525f4",
+        "background-light": "#f5f5f8",
+        "background-dark": "#101022",
+      },
+      fontFamily: {
+        display: ["Inter"],
+      },
+      borderRadius: {
+        DEFAULT: "0.25rem",
+        lg: "0.5rem",
+        xl: "0.75rem",
+        full: "9999px",
+      },
+    },
+  },
+};
